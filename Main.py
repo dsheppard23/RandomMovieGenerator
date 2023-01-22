de520d6fa03c8a5d007921f7e0464d3f94720dac
Dillon Sheppard <dillonsheppard23@outlook.com>
2023-01-22 17:08:03 -0500

    partial code to download button

--- Main.py
+++ Main.py
@@ -1,12 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox
 from selenium import webdriver
+import time
 
 def upload_watchlist():
     messagebox.showinfo("Watchlist Manager", "Uploading watchlist...")
 
 def download_watchlist():
-    messagebox.showinfo("Watchlist Manager", "Downloading watchlist...")
+    # Create a new instance of the Safari driver
+    driver = webdriver.Safari()
+
+    # Navigate to the IMDb website
+    driver.get("https://www.imdb.com/")
+
+    #wait for the page to load
+    time.sleep(5)
+    # Code to log in and download the watchlist goes here
+    
+    # Close the browser
+    driver.quit()
+
 
 def CreateGUI():
     root = tk.Tk()
